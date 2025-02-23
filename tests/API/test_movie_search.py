2d62c37c0ebd613945c78c877b1122a1f0208585
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2025-02-23 23:27:43 +0300

    add update

--- tests/API/test_movie_search.py
+++ tests/API/test_movie_search.py
@@ -12,7 +12,7 @@ def test_movie_search_api_validate_response(base_api_url):
     params = {"apikey": "a20b12b279f744f2b3c7b5c5400c4eb5",
               "q": "жить жизнь",
               "locale": "ru",
-              "limit": "20",
+              "limit": "40",
               "offset": 0
               }
 
@@ -20,8 +20,8 @@ def test_movie_search_api_validate_response(base_api_url):
 
     with allure.step('Status code=200'):
         assert response.status_code == 200
-    with allure.step('alias = zhitzhizn'):
-        assert response.json()["items"][0]["alias"] == "zhitzhizn"
+    with allure.step('alias = zhit-zhizn'):
+        assert response.json()["items"][0]["alias"] == "zhit-zhizn-1-sezon"
     with allure.step('Validate response schema'):
         body = response.json()
         validate(body, search_movie_schema)
