9ce89d4d8146384eb6d7b0580dcfdd357fec32a8
Anastasiya_Lavretskaya <a.lavretskaya87@yandex.ru>
2025-03-06 14:13:22 +0300

    Update files

--- qa_guru_python17_diploma_work/schemas/schemas.py
+++ qa_guru_python17_diploma_work/schemas/schemas.py
@@ -9148,7 +9148,7 @@ search_movie_schema = {
                   "type": "null"
                 },
                 "image_15x": {
-                  "type": "string"
+                  "type": "null"
                 }
               },
               "required": [
